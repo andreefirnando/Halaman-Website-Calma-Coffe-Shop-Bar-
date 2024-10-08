da9f375093c3a945aa18f733e630ba92ae8b4d8a
M Akbar Andre Firnando <60153389+andreefirnando@users.noreply.github.com>
2024-10-08 10:23:02 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// JavaScript to toggle the mobile navigation menu
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+  menuToggle.classList.toggle("active");
+  navLinks.classList.toggle("active");
+});
